ac44d454307fc4f69595f8ba11d541356f80ca8b
Irina <pamir.08@mail.ru>
2022-10-14 15:16:08 +0300

    ex1

---
+++ ex1/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задать массив из 8 элементов, заполненных нулями и единицами вывести их на экран
+
+void PrintArray (int[] array)
+{
+    int count = array.Length;
+    for (int i = 0; i < count; i++)
+    {
+        Console.Write ($"{array[i]}");
+    }
+    Console.WriteLine ();
+
+}
+int [] array = {0,1,0,0,0,1,1,0};
+
+PrintArray (array);
