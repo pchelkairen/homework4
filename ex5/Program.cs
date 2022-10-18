b823332f73c5cffb835cec8345ab8a57f69610b0
Irina <pamir.08@mail.ru>
2022-10-18 18:37:49 +0300

    ex5

---
+++ ex5/Program.cs
@@ -0,0 +1,19 @@
+﻿// Найти сумму чисел одномерного массива стоящих на нечетной позиции
+int size = 10;
+int[] array = new int[size];
+int count = 0;
+int sum = 0;
+for (int index = 0; index < size; index++)
+    array[index] = new Random().Next(1,5);
+
+Console.WriteLine("Ваш массив ");
+
+for (int index = 0; index < size; index++)
+        Console.Write ($"{array[index]} ");
+
+
+for (int index = 0; index < size; index++)
+    if (index % 2 != 0)
+    sum = sum + 1;
+    
+Console.WriteLine($"Cуммa чисел одномерного массива стоящих на нечетной позиции равнa {sum}");
